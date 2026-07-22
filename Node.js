b9b3b7da4a93db446aeffcdd870e37e574d4d357
js-blord <jstring1482@gmail.com>
2026-07-22 16:25:11 -0400

    Add body parser middleware to fix 405 error

--- Node.js
+++ Node.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+// Parse incoming request bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.post('/login', (req, res) => {
     const { uname, psw } = req.body;
     
